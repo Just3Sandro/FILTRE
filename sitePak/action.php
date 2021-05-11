41d04eaa19c4a3270fe3622cd6c9a7a635b64817
Stalobile <73789401+Stalobile@users.noreply.github.com>
2021-05-11 18:43:12 +0200

    Add files via upload
    blblbl

--- sitePak/action.php
+++ sitePak/action.php
@@ -1,23 +1,24 @@
 <?php 
-// on connecte notre bdd avec le fichier db.php
 require 'db.php';
-// vérifie que les données existe bien plus déclaration
+
 if (isset($_POST['action'])){
-    $sql = "SELECT * FROM vehicule,energie WHERE vehicule.typeEnergie = energie.codeEnergie AND marque !=''";
+    $sql = "SELECT * FROM vehicule,energie WHERE vehicule.typeEnergie = energie.codeEnergie AND marque !='' ";
+
+
 
     if(isset($_POST['marque'])){
         $marque = implode("','", $_POST['marque']);
         $sql .="AND marque IN('".$marque."')";
     }
- 
+
     if(isset($_POST['codeEnergie'])){
         $codeEnergie = implode("','", $_POST['codeEnergie']);
         $sql .="AND codeEnergie IN('".$codeEnergie."')";
     }
-  
+
     $result = $conn->query($sql);
     $output='';
-    //Si une donnée est trouvé on affiche ça
+
     if($result->num_rows>0){
         while($row=$result->FETCH_ASSOC()){
             $output .='    <div class="col-md-3 mb-2">
@@ -45,11 +46,10 @@ if (isset($_POST['action'])){
         ';
         }
     }
-    // si rien est trouvé on affiche ça 
     else{
         $output = "<h3> PAS DE VEHICULE TROUVé";
     }
     echo $output;
 }
 
-?>
+?> 
--- sitePak/index.php
+++ sitePak/index.php
@@ -2,14 +2,12 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>L'autoroutier</title>
-
-    <!-- on déclare du BOOTSTRAP et du javascript -->
+    <title>LA KIFFANCE</title>
     <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
@@ -21,22 +19,17 @@
 
 <!-- Latest compiled JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-<!-- Déclare du css -->
-<link rel="stylesheet" href="css/style.css" >
-
 </head>
 <body>
-    <div class="text-center text-light bg-info p-2">
-        <h3 >L'autoroutier</H3>
-    </div>
+    <h3 class="text-center text-light bg-info p-2">LA KIFFANCE</h3>
     <div class="container-fluid">
         <div class="row">
             <div class="col-lg-3"> 
-            <h5>Produit Filtrer</h5>
+            <h5>Filter product</h5>
             <hr>
-            <!-- Début du formulaire-->
+
             <form action="" method="POST">
-            <!-- Zone de prix avec un prix de base -->
+
             <div class="col-md-4">
                 <label for="">Prix Mini</label>
                 <input type="text" name="prix_mini" value="<?php if(isset($_POST['prix_mini'])){echo $_POST['prix_mini']; }else{echo"300";} ?>" class="form-control">
@@ -52,30 +45,25 @@
             </form>
             
 
-            <h6 class="text-info">Selection du Vehicule</h6>
+            <h6 class="text-info">Select Vehicule</h6>
             <ul class="list-group">
-                <!-- séléction du vehiclue-->
                 <?php 
                     $sql="SELECT DISTINCT marque FROM vehicule";
                     $result=$conn->query($sql);
-                    // ouveture de la boucle while pour afficher toutes les voitures de la bdd
                     while($row=$result->FETCH_ASSOC()){
                 ?>
                 <li class="list-group-item">
                     <div class="from-check">
                         <label class="form-check-label">
-                            <!-- echo du resultat ($row), on lui mes la valeur 'marque', une class et son nom -->
-                            <input type="checkbox" class="form-check-input product_check" value="<?= $row['marque']; ?>" class="marque"><?= $row['marque']; ?> 
+                            <input type="checkbox" class="form-check-input product_check" value="<?= $row['marque']; ?>" id="marque"><?= $row['marque']; ?> 
                         </label>
                     </div>
                 </li>
-                <!--fermeture boucle while -->
                 <?php } ?>
             </ul>
 
-            <h6 class="text-info">Selection de energie</h6>
+            <h6 class="text-info">Select energie</h6>
             <ul class="list-group">
-            <!-- même proceder que au-dessus-->
                 <?php 
                     $sql="SELECT DISTINCT codeEnergie, libelleEnergie FROM vehicule,energie WHERE vehicule.typeEnergie = energie.codeEnergie";
                     $result=$conn->query($sql);
@@ -90,23 +78,18 @@
                 </li>
                 <?php } ?>
             </ul>
-            <!-- bouton pour acceder au formulaire-->
-            <a class="btn btn-primary" href="formulaire.php" role="button">Suggestion ?</a>
-
             </div>
-
             <div class="col-lg-9">
                 <h5 class="text-center" id="textChange"> TOUS LES PRODUITS</h5>
                 <hr>
                 <div class="row" id="result">
-                <!-- vérifie que les prix soit bien et on les déclare dans une variable -->
                 <?php   
                     if(isset($_POST['prix_mini'])&& isset($_POST['prix_max']))
                     {
                         $petitprix=$_POST['prix_mini'];
                         $grandprix=$_POST['prix_max'];
                     }
-                    //On selectionne ce que l'on veut afficher
+
                     $sql="SELECT * FROM vehicule,energie WHERE vehicule.typeEnergie = energie.codeEnergie AND prixvente BETWEEN $petitprix AND $grandprix";
                     $result=$conn->query($sql);
                     while($row=$result->FETCH_ASSOC()){
@@ -114,14 +97,12 @@
                 <div class="col-md-3 mb-2">
                     <div class="card-deck">
                         <div class="card border-secondary">
-                        <!-- Ajout des images-->
                             <img src="<?= $row['photo']; ?>"class="card-img-top">
                             <div class="card-img-overlay">
                             </div>
-                            <!-- Ajout nom de la voiture -->
                             <h6  class="text-light bg-info text-center rounded p-1"><?= $row['description']; ?><h6>
 
-                            <!-- On ajoute des information sur les voitures-->
+                            
                             <div class="card-body">
                                 <h4 class="card-title text-danger"> Prix: <?= number_format($row['prixvente']); ?>/- </h4>
                                 <p>
@@ -142,10 +123,9 @@
             </div>
         </div>
     </div>
-    <!-- javascript !!-->
     <script type="text/javascript">
     $(document).ready(function(){
-        //Si le produit est check
+
         $('.product_check').click(function(){
            var action = 'data';
            var marque = get_filter_text('marque'); 
@@ -172,6 +152,5 @@
 
     });
     </script>
-
 </body>
 </html>
